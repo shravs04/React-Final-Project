351d9aecc97a4df407714cb387ece44ead144e82
shravs04 <sgajula55@gmail.com>
2020-10-16 11:31:06 +0530

    Enter

--- react-final-project/src/App.js
+++ react-final-project/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import TopBar from './Components/TopBar/TopBar';
 import ProblemListingPage from './Containers/ProblemListingPage/ProblemListingPage';
 import Footer from './Components/Footer/Footer';
-=======
->>>>>>> c0464f571ca39974a1a44d1e959eba477bb61801
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-        {/* <TopBar/>
+        <TopBar/>
         <ProblemListingPage/>
-        <Footer/> */}
-=======
->>>>>>> c0464f571ca39974a1a44d1e959eba477bb61801
+        <Footer/>
     </div>
   );
 }
